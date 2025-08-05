a7e9ed2553fe92fb860d90eec975a4aeff833bac
jingan <jing.an@sweet.io>
2025-08-05 10:52:45 +0100

    update for batch mint and fixes

---
+++ examples/nft/standard/sweet_batchmint.py
@@ -0,0 +1,59 @@
+import uuid
+from typing import Union
+
+from pytoniq_core import Address
+
+from tonutils.client import TonapiClient
+from tonutils.nft.contract.standard.collection import SweetCollectionStandard
+from tonutils.nft.contract.standard.nft import SweetNFTStandard
+from tonutils.wallet import WalletV5R1
+
+# API key for accessing the Tonapi (obtainable from https://tonconsole.com)
+API_KEY = ""
+
+# Set to True for test network, False for main network
+IS_TESTNET = True
+
+# Mnemonic phrase used to connect the wallet, should be minter or owner of the NFT collection
+MNEMONIC: list[str] = []
+
+# Address of the owner of the NFT and the NFT collection contract
+
+OWNER_ADDRESS_1 = ""
+OWNER_ADDRESS_2 = ""
+
+COLLECTION_ADDRESS = ""
+
+
+def uuid_to_query_id(txn_id: Union[str, uuid.UUID]) -> int:
+    if isinstance(txn_id, str):
+        txn_id = uuid.UUID(txn_id)
+    return txn_id.int & 0xFFFFFFFFFFFFFFFF  # Use lower 64 bits for query_id
+
+
+async def main() -> None:
+    client = TonapiClient(api_key=API_KEY, is_testnet=IS_TESTNET)
+    wallet, _, _, _ = WalletV5R1.from_mnemonic(client, MNEMONIC)
+
+    nft = SweetNFTStandard(
+        collection_address=Address(COLLECTION_ADDRESS),
+    )
+
+    body = SweetCollectionStandard.build_batch_mint_body(
+        addresses=[Address(OWNER_ADDRESS_1), Address(OWNER_ADDRESS_2)],
+    )
+
+    tx_hash = await wallet.transfer(
+        destination=COLLECTION_ADDRESS,
+        amount=0.2,
+        body=body,
+    )
+
+    print(f"Successfully minted NFT from collection {nft.address.to_str()}")
+    print(f"Transaction hash: {tx_hash}")
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(main())
--- tonutils/nft/contract/standard/collection.py
+++ tonutils/nft/contract/standard/collection.py
@@ -1,53 +1,54 @@
 from __future__ import annotations
 
-from typing import List, Tuple, Union
+from typing import List, Optional, Tuple, Union
 
 from pytoniq_core import Address, Cell, HashMap, begin_cell
 
-from .nft import NFTStandard
-from ..base.collection import Collection, get_gas_fee
 from ...content import (
-    CollectionOffchainContent,
-    CollectionModifiedOnchainContent,
     CollectionModifiedOffchainContent,
-    NFTOffchainContent,
-    NFTModifiedOnchainContent,
+    CollectionModifiedOnchainContent,
+    CollectionOffchainContent,
     NFTModifiedOffchainContent,
+    NFTModifiedOnchainContent,
+    NFTOffchainContent,
     SweetOffchainContent,
 )
 from ...data import CollectionData
 from ...op_codes import *
 from ...royalty_params import RoyaltyParams
+from ..base.collection import Collection, get_gas_fee
+from .nft import NFTStandard
 
 
 class CollectionStandardBase(Collection):
-
     def __init__(
-            self,
-            owner_address: Address,
-            next_item_index: int,
-            content: Union[
-                CollectionOffchainContent,
-                CollectionModifiedOnchainContent,
-                CollectionModifiedOffchainContent,
-                SweetOffchainContent,
-            ],
-            royalty_params: RoyaltyParams,
+        self,
+        owner_address: Address,
+        next_item_index: int,
+        content: Union[
+            CollectionOffchainContent,
+            CollectionModifiedOnchainContent,
+            CollectionModifiedOffchainContent,
+            SweetOffchainContent,
+        ],
+        royalty_params: RoyaltyParams,
     ) -> None:
-        self._data = self.create_data(owner_address, next_item_index, content, royalty_params).serialize()
+        self._data = self.create_data(
+            owner_address, next_item_index, content, royalty_params
+        ).serialize()
         self._code = Cell.one_from_boc(self.CODE_HEX)
 
     @classmethod
     def create_data(
-            cls,
-            owner_address: Address,
-            next_item_index: int,
-            content: Union[
-                CollectionOffchainContent,
-                CollectionModifiedOnchainContent,
-                CollectionModifiedOffchainContent,
-            ],
-            royalty_params: RoyaltyParams,
+        cls,
+        owner_address: Address,
+        next_item_index: int,
+        content: Union[
+            CollectionOffchainContent,
+            CollectionModifiedOnchainContent,
+            CollectionModifiedOffchainContent,
+        ],
+        royalty_params: RoyaltyParams,
     ) -> CollectionData:
         return CollectionData(
             owner_address=owner_address,
@@ -59,11 +60,11 @@ class CollectionStandardBase(Collection):
 
     @classmethod
     def build_mint_body(
-            cls,
-            index: int,
-            owner_address: Address,
-            amount: int = 20000000,
-            query_id: int = 0,
+        cls,
+        index: int,
+        owner_address: Address,
+        amount: int = 20000000,
+        query_id: int = 0,
     ) -> Cell:
         """
         Builds the body of the mint transaction.
@@ -81,25 +82,26 @@ class CollectionStandardBase(Collection):
             .store_uint(query_id, 64)
             .store_uint(index, 64)
             .store_coins(amount)
-            .store_ref(
-                begin_cell()
-                .store_address(owner_address)
-                .end_cell()
-            )
+            .store_ref(begin_cell().store_address(owner_address).end_cell())
             .end_cell()
         )
 
     @classmethod
     def build_batch_mint_body(
-            cls,
-            data: List[Tuple[Union[
-                NFTOffchainContent,
-                NFTModifiedOnchainContent,
-                NFTModifiedOffchainContent,
-            ], Address]],
-            from_index: int,
-            amount_per_one: int = 20000000,
-            query_id: int = 0,
+        cls,
+        data: List[
+            Tuple[
+                Union[
+                    NFTOffchainContent,
+                    NFTModifiedOnchainContent,
+                    NFTModifiedOffchainContent,
+                ],
+                Address,
+            ]
+        ],
+        from_index: int,
+        amount_per_one: int = 20000000,
+        query_id: int = 0,
     ) -> Cell:
         """
         Builds the body of the batch mint transaction.
@@ -125,7 +127,7 @@ class CollectionStandardBase(Collection):
                     .store_ref(content.serialize())
                     .end_cell()
                 )
-                .end_cell()
+                .end_cell(),
             )
 
         return (
@@ -141,11 +143,11 @@ class CollectionStandard(CollectionStandardBase):
     CODE_HEX = "b5ee9c72410213010001fe000114ff00f4a413f4bcf2c80b0102016204020201200e030025bc82df6a2687d20699fea6a6a182de86a182c40202cd0a050201200706003d45af0047021f005778018c8cb0558cf165004fa0213cb6b12ccccc971fb0080201200908001b3e401d3232c084b281f2fff27420002d007232cffe0a33c5b25c083232c044fd003d0032c0326003ebd10638048adf000e8698180b8d848adf07d201800e98fe99ff6a2687d20699fea6a6a184108349e9ca829405d47141baf8280e8410854658056b84008646582a802e78b127d010a65b509e58fe59f80e78b64c0207d80701b28b9e382f970c892e000f18112e001718119026001f1812f82c207f97840d0c0b002801fa40304144c85005cf1613cb3fccccccc9ed5400a6357003d4308e378040f4966fa5208e2906a4208100fabe93f2c18fde81019321a05325bbf2f402fa00d43022544b30f00623ba9302a402de04926c21e2b3e6303250444313c85005cf1613cb3fccccccc9ed5400603502d33f5313bbf2e1925313ba01fa00d43028103459f0068e1201a44343c85005cf1613cb3fccccccc9ed54925f05e2020120120f0201201110002db4f47da89a1f481a67fa9a9a86028be09e008e003e00b0002fb5dafda89a1f481a67fa9a9a860d883a1a61fa61ff4806100043b8b5d31ed44d0fa40d33fd4d4d43010245f04d0d431d430d071c8cb0701cf16ccc98f34ea10e"  # noqa
 
     def __init__(
-            self,
-            owner_address: Address,
-            next_item_index: int,
-            content: CollectionOffchainContent,
-            royalty_params: RoyaltyParams,
+        self,
+        owner_address: Address,
+        next_item_index: int,
+        content: CollectionOffchainContent,
+        royalty_params: RoyaltyParams,
     ) -> None:
         super().__init__(
             owner_address=owner_address,
@@ -156,11 +158,11 @@ class CollectionStandard(CollectionStandardBase):
 
     @classmethod
     def build_mint_body(
-            cls,
-            index: int,
-            owner_address: Address,
-            amount: int = 20000000,
-            query_id: int = 0,
+        cls,
+        index: int,
+        owner_address: Address,
+        amount: int = 20000000,
+        query_id: int = 0,
     ) -> Cell:
         return super().build_mint_body(
             index=index,
@@ -171,11 +173,11 @@ class CollectionStandard(CollectionStandardBase):
 
     @classmethod
     def build_batch_mint_body(
-            cls,
-            data: List[Tuple[NFTOffchainContent, Address]],
-            from_index: int,
-            amount_per_one: int = 20000000,
-            query_id: int = 0,
+        cls,
+        data: List[Tuple[NFTOffchainContent, Address]],
+        from_index: int,
+        amount_per_one: int = 20000000,
+        query_id: int = 0,
     ) -> Cell:
         return super().build_batch_mint_body(
             data=data,
@@ -190,11 +192,13 @@ class CollectionStandardModified(CollectionStandardBase):
     CODE_HEX = "b5ee9c724102140100020d000114ff00f4a413f4bcf2c80b0102016202030202cc04050201200e0f04e7d90638048adf000e8698180b8d848adf07d201800e98fe99ff6a2687d20699fea6a6a184108349e9ca829405d47141baf8280e8410854658056b84008646582a802e78b127d010a65b509e58fe59f80e78b64c0207d807029c26382f970c893e000f18113e00171811a136001f1812f8290e002c060708090201480a0b006436363602d33f5313bbf2e1925313ba01fa00d43027103459f00b8e1201a45521c85005cf1613cb3fccccccc9ed54925f05e200a63636367003d4308e378040f4966fa5208e2906a4208100fabe93f2c18fde81019321a05325bbf2f402fa00d43022544a30f00b23ba9302a402de04926c21e2b3e630324434c85005cf1613cb3fccccccc9ed54002e35353501fa40305530c85005cf1613cb3fccccccc9ed5400548e21708018c8cb055004cf1623fa0213cb6acb1fcb3f820afaf08070fb02c98306fb00e05f03840ff2f0002d501c8cb3ff828cf16c97020c8cb0113f400f400cb00c980201200c0d001b3e401d3232c084b281f2fff27420003d16bc025c087c029de0063232c15633c594013e8084f2dac4b333325c7ec0200201201011001fbc82df6a2687d20699fea6a6a182dac40007b8b5d3180201201213002fb5dafda89a1f481a67fa9a9a860d883a1a61fa61ff480610002db4f47da89a1f481a67fa9a9a86028be09e012e003e01500a97eda5"  # noqa
 
     def __init__(
-            self,
-            owner_address: Address,
-            next_item_index: int,
-            content: Union[CollectionModifiedOnchainContent, CollectionModifiedOffchainContent],
-            royalty_params: RoyaltyParams,
+        self,
+        owner_address: Address,
+        next_item_index: int,
+        content: Union[
+            CollectionModifiedOnchainContent, CollectionModifiedOffchainContent
+        ],
+        royalty_params: RoyaltyParams,
     ) -> None:
         super().__init__(
             owner_address=owner_address,
@@ -205,11 +209,11 @@ class CollectionStandardModified(CollectionStandardBase):
 
     @classmethod
     def build_mint_body(
-            cls,
-            index: int,
-            owner_address: Address,
-            amount: int = 20000000,
-            query_id: int = 0,
+        cls,
+        index: int,
+        owner_address: Address,
+        amount: int = 20000000,
+        query_id: int = 0,
     ) -> Cell:
         return super().build_mint_body(
             index=index,
@@ -220,11 +224,13 @@ class CollectionStandardModified(CollectionStandardBase):
 
     @classmethod
     def build_batch_mint_body(
-            cls,
-            data: List[Tuple[Union[NFTModifiedOnchainContent, NFTModifiedOffchainContent], Address]],
-            from_index: int,
-            amount_per_one: int = 20000000,
-            query_id: int = 0,
+        cls,
+        data: List[
+            Tuple[Union[NFTModifiedOnchainContent, NFTModifiedOffchainContent], Address]
+        ],
+        from_index: int,
+        amount_per_one: int = 20000000,
+        query_id: int = 0,
     ) -> Cell:
         return super().build_batch_mint_body(
             data=data,
@@ -254,11 +260,11 @@ class SweetCollectionStandard(CollectionStandardBase):
     CODE_HEX = "b5ee9c7241021401000255000114ff00f4a413f4bcf2c80b01020162020d0202cc030c020120040904edd10638048adf000e8698180b8d848adf07d201800e98fe99ff6a2687d20699fea6a6a184108349e9ca829405d47141baf8280e8410854658056b84008646582a802e78b127d010a65b509e58fe59f80e78b64c0207d80701b28b9e382f970c8b8a9305d71813929305d718139a9305d7181181aba0a5d405060708006635547000ba20997a5230ba93f2c18fdede04fa00d4302859f008028e1201a44343c85005cf1613cb3fccccccc9ed54925f05e200a6357003d4308e378040f4966fa5208e2906a45304a07aba93f2c18fde81019321a05325bbf2f402fa00d43022544b30f00823ba9302a402de04926c21e2b3e6303250444313c85005cf1613cb3fccccccc9ed540052323401fa40302020d749c10292307095d30130c300e2f2e1954144c85005cf1613cb3fccccccc9ed5400628e28d421d0d749830bbbf2e25bd43020d0d749830cbbf2e25d103458c85005cf1613cb3fccccccc9ed54e05f04840ff2f00201580a0b002d007232cffe0a33c5b25c083232c044fd003d0032c03260001b3e401d3232c084b281f2fff274200051d90686ba4c185ddf9712cad78033810f803bbc00c646582ac678b28027d0109e5b589666664b8fd8040201200e130201200f100043b8b5d31ed44d0fa40d33fd4d4d43010245f04d0d431d430d071c8cb0701cf16ccc980201201112002fb5dafda89a1f481a67fa9a9a860d883a1a61fa61ff480610002db4f47da89a1f481a67fa9a9a86028be09e00ce003e00f00025bc82df6a2687d20699fea6a6a182de86a182c49d2cb760"
 
     def __init__(
-            self,
-            owner_address: Address,
-            next_item_index: int,
-            content: SweetOffchainContent,
-            royalty_params: RoyaltyParams,
+        self,
+        owner_address: Address,
+        next_item_index: int,
+        content: SweetOffchainContent,
+        royalty_params: RoyaltyParams,
     ) -> None:
         super().__init__(
             owner_address=owner_address,
@@ -269,11 +275,11 @@ class SweetCollectionStandard(CollectionStandardBase):
 
     @classmethod
     def build_mint_body(
-            cls,
-            owner_address: Address,
-            amount: int = get_gas_fee(),
-            query_id: int = 0,
-            admin_mint: Optional[bool] = False,
+        cls,
+        owner_address: Address,
+        amount: int = get_gas_fee(),
+        query_id: int = 0,
+        admin_mint: Optional[bool] = False,
     ) -> Cell:
         """
         Builds the body of the mint transaction.
@@ -289,25 +295,37 @@ class SweetCollectionStandard(CollectionStandardBase):
             .store_uint(op_code, 32)
             .store_uint(query_id, 64)
             .store_coins(amount)
-            .store_ref(
-                begin_cell()
-                .store_address(owner_address)
-                .end_cell()
-            )
+            .store_ref(begin_cell().store_address(owner_address).end_cell())
             .end_cell()
         )
 
     @classmethod
     def build_batch_mint_body(
-            cls,
-            data: List[Tuple[Union[
-                NFTOffchainContent,
-                NFTModifiedOnchainContent,
-                NFTModifiedOffchainContent,
-            ], Address]],
-            from_index: int,
-            amount_per_one: int = 20000000,
-            query_id: int = 0,
+        cls,
+        addresses: List[Address],
+        amount_per_one: int = 20000000,
+        query_id: int = 0,
     ) -> Cell:
-        # We are not supporting batch minting for SweetCollectionNFT for the moment.
-        raise NotImplementedError
+
+        items_dict = HashMap(key_size=64)
+
+        for i, owner_address in enumerate(addresses, start=0):
+            items_dict.set_int_key(
+                i,
+                begin_cell()
+                .store_ref(
+                    begin_cell()
+                    .store_ref(begin_cell().store_address(owner_address).end_cell())
+                    .store_coins(amount_per_one)
+                    .end_cell()
+                )
+                .end_cell(),
+            )
+
+        return (
+            begin_cell()
+            .store_uint(BATCH_NFT_MINT_OPCODE, 32)
+            .store_uint(query_id, 64)
+            .store_dict(items_dict.serialize())
+            .end_cell()
+        )
